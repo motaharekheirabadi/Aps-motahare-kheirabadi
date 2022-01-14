eae5be20db4eee9c2e88136e43484ea294528476
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-01-14 11:38:38 +0330

    chenge file

---
+++ folder-1/practice.js
@@ -0,0 +1,95 @@
+for(let i = 1 ; i <= 10 ; i++){
+    // console.log(i,'MOTI')
+}
+
+for( let i = 1 ; i<= 6 ; i++){
+    // console.log(i,'in shabi k migan shab nist age shabe mes dishab nis');
+}
+
+for(let i = 0 ; i<= 5 ; i++){
+    // console.log(`${i} in shabi ke migan shan nist ...`)
+}
+
+for(let i = 0 ; i <= 20 ; i+=2){
+// console.log(i);
+}
+
+for(let i = 1 ;i <= 20 ;i += 2){
+    // console.log(i)
+}
+
+for(let i = 100 ; i>= 0; i--){
+    // console.log(i)
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+for(let i = 50; i>= 0 ; i-=10){
+// console.log(i)
+}
+
+for(let i = 25 ; i>=0 ;i-=5){
+// console.log(i)
+}
+///////////////////////////////////////////////////////////////////////////////
+
+for(let i = 20 ; i>= 0 ; i++){
+// console.log(i)
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+let names = ["mohammad", "romina", "mohammadHossein"];
+// console.log(names.length);
+// console.log(names[names.length - 1]);
+
+
+///////////////////////////////////////////////////////////////////////////
+
+let students = ["Sirvan", "Pourya", "Alireza", "saba", "Khorzoor"];
+
+for(let i=0 ; i < students.length ; i++){
+
+    // console.log(students[i].toUpperCase())
+// }
+
+/////////////////////////////////////////////////////////////////////////
+
+const airplane = [
+  ["niloofar", "kosar", "Fatemeh"],
+  ["ali", "mohsen", "Shirin"],
+  ["atena", "mahdi", "benyamin"],
+  ["bani", "mohammad", "sina"],
+  ["Amir", "Beti", "Pourya"],
+];
+
+for (let i = 0; i < airplane.length; i++) {
+  // console.log(i+1 )
+  // console.log(airplane[i])
+
+  for (let j = 0; j < airplane[i].length; j++) {
+    // console.log(airplane[i][j]);
+  }
+}
+
+let count = 0;
+while (count <= 10) {
+  //   console.log(count);
+
+  count++;
+}
+
+///////////////////////////////////////////////////////////////////
+
+let kitten = {
+  ageMonths: 3,
+  isFemale: true,
+  furColour: "brown",
+};
+// with dot notation
+console.log(kitten.ageMonths);
+console.log(kitten.isFemale);
+console.log(kitten.furColour);
+
+}
