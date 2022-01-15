9c82a1ef18e74d66088bb938707be14caa95f4b9
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-01-15 18:53:20 +0330

    add htmlfile

--- folder-2-js1-2/script.js
+++ folder-2-js1-2/script.js
@@ -3,9 +3,29 @@
 let dice1 = Math.floor(Math.random()*6 + 1)
 let dice2 = Math.floor(Math.random()*6 + 1)
 
-console.log('dice1:' , dice1);
-console.log('dice2:' , dice2);
+// console.log('dice1:' , dice1);
+// console.log('dice2:' , dice2);
 
 let roll = `you rolled a ${dice1} and a ${dice2} .they sum to a ${dice1 + dice2}`
 
-console.log(roll);
+// console.log(roll);
+
+// alert('welcome ')
+
+// let num = prompt('what is your name?')
+
+// console.log(num);
+
+let input = prompt("what is your age ?")
+let age = parseInt(input)
+
+let birthday = 1400 - age 
+
+alert (`your birthday is ${birthday}`)
+
+// console.log(birthday);
+
+
+
+
+
