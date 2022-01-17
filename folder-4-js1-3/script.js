9be12e93b97d03c539f01d0ed1da1daefe5dd440
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-01-17 17:48:41 +0330

    commit

---
+++ folder-4-js1-3/script.js
@@ -0,0 +1,32 @@
+//! 27/10/1400
+
+const numbers =[1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ]
+
+for( num of numbers){
+    // console.log(num**2);
+}
+
+for (let i=0 ; i <= 20 ; i += 2){
+    // console.log(i);
+}
+
+
+const arr = [1,2,3,4]
+ let j=0
+for(i=0 ;i<arr.length;i++ ){
+
+    j +=arr[i]
+}
+
+// console.log(j);
+
+for(let i=1; i<= 20 ; i+= 2){
+    // console.log(i);
+}
+
+for(let i=50 ; i>=0 ; i-= 10){
+    console.log(i);
+}
+
+
+
