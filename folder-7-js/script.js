e6078397b0cd8be0ed16fc1ab28e2260e4a03e01
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-15 16:09:30 +0330

    chenge

--- folder-7-js/script.js
+++ folder-7-js/script.js
@@ -30,4 +30,25 @@ document
 
     return `rgb(${r} , ${g} , ${b})`;
   };
-  
+
+///////////////////////////////////////////////////////////////////////////
+
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+// 
+  return `rgb(${r} , ${g} , ${b})`;
+};
+// 
+const buttons = document.querySelectorAll("button");
+// 
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    console.log(this);
+// 
+    // 
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
+  });
+}
