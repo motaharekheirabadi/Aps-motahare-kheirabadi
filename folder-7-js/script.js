37e4696ddc0c5d6cff6ec14d5d48a4aeb5a44181
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-15 13:01:43 +0330

    chengefile

---
+++ folder-7-js/script.js
@@ -0,0 +1,33 @@
+//! 25/11/1400
+//* js2-3 - review
+
+const hello = document.querySelector("#hello");
+hello.addEventListener("click", () => console.log("hello"));
+
+document
+  .querySelector("#goodbye")
+  .addEventListener("click", () => console.log("goodbye"));
+
+  /////////////////////////////////////////////////////////////////////
+
+  const button = document.querySelector("button");
+  const h1 = document.querySelector("h1");
+  const p = document.createElement("p");
+
+  h1.append(p);
+
+
+  button.addEventListener("click", () => {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+    p.innerText = newColor;
+  });
+
+  const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r} , ${g} , ${b})`;
+  };
+  
