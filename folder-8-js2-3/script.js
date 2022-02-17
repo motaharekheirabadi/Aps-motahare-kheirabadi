a4d6ee22bcfa204559d5af88d82737afdfb27b6e
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-17 23:11:43 +0330

    chenge

--- folder-8-js2-3/script.js
+++ folder-8-js2-3/script.js
@@ -1,37 +1,37 @@
 //! 28/11/1400 
 
-const form = document.querySelector("form");
-const list = document.querySelector("ul");
+// const form = document.querySelector("form");
+// const list = document.querySelector("ul");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  const product = e.target.product;
-  const quantity = e.target.qty;
+//   const product = e.target.product;
+//   const quantity = e.target.qty;
 
-  const li = document.createElement("li");
-  li.textContent = `${product.value} : ${quantity.value}`;
+//   const li = document.createElement("li");
+//   li.textContent = `${product.value} : ${quantity.value}`;
 
-  list.appendChild(li);
+//   list.appendChild(li);
 
-  product.value = "";
-  quantity.value = "";
-});
+//   product.value = "";
+//   quantity.value = "";
+// });
 
 ////////////////////////////////////////////////////////////////////////////
 
 //?  chenge and inputEvents
 
-const input = document.querySelector("input");
-const h1 = document.querySelector("h1");
+// const input = document.querySelector("input");
+// const h1 = document.querySelector("h1");
 
 // input.addEventListener("change", (e) => {
 //   console.log("hi aps");
 // });
 
-input.addEventListener("input", (e) => {
-  h1.innerText = input.value;
-});
+// input.addEventListener("input", (e) => {
+//   h1.innerText = input.value;
+// });
 
 //////////////////////////////////////////////////////////////////
     
