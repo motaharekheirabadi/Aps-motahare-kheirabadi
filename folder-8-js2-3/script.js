cbb57e1e0c8c693024ae286e606d540e028fc672
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-18 22:35:27 +0330

    chengefile

--- folder-8-js2-3/script.js
+++ folder-8-js2-3/script.js
@@ -137,6 +137,23 @@ delayedColorChange("khaki", 5000);
 
 
 
+ const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext && doNext();
+  }, delay);
+};
+
+delayedColorChange("red", 1000, () => {
+  delayedColorChange("orange", 1000, () => {
+    delayedColorChange("yellow", 1000);
+  });
+});
+
+
+
+
+
 
 
 
