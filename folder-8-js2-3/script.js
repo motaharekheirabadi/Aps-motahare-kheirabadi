dd137a769eb16334396fbe730f177b36a567d19e
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-18 12:02:41 +0330

    bubbling

--- folder-8-js2-3/script.js
+++ folder-8-js2-3/script.js
@@ -1,4 +1,4 @@
-//! 28/11/1400 
+//! 28/11/1400  & 29/11/1400
 
 // const form = document.querySelector("form");
 // const list = document.querySelector("ul");
@@ -41,4 +41,39 @@ const input = document.querySelector("#username");
 input.addEventListener("input", (e) => {
   h1.textContent = `welcome: ${e.target.value}`;
 });
+
+///////////////////////////////////////////////////////////////////////
+
+const button = document.querySelector("#changeColor");
+const container = document.querySelector("#container");
+
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+button.addEventListener("click", (e) => {
+  //   e.stopPropagation();
+  e.stopImmediatePropagation();
+  let color = makeRandColor();
+  container.style.backgroundColor = color;
+  console.log(color);
+
+  console.log("1");
+});
+
+button.addEventListener("click", (e) => {
+  let color = makeRandColor();
+  container.style.backgroundColor = color;
+  console.log(color);
+  console.log("2");
+});
+
+container.addEventListener("click", () => {
+  container.classList.toggle("hide");
+});
+
+////////////////////////////////////////////////////////////
 
