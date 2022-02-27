d551a25d371269bf9d57f3b6e79a6496df7f8b4b
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-02-27 22:34:06 +0330

    addfetch

--- folder-11-js3-1/script.js
+++ folder-11-js3-1/script.js
@@ -42,5 +42,35 @@ req.send();
 
 ///////////////////////////////////////////////////////////////////////////
 
+const fetchUsers = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await res.json();
+    console.log(data);
+  } catch (jasemError) {
+    console.log("something went wrong", jasemError);
+  }
+};
+
+fetchUsers();
+
+
+ fetch("https://apitester.ir/api/Categories")
+  .then((data) => data.json())
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+const fetchUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  console.log(res);
+  const data = await res.json();
+  return data;
+};
+
+fetchUsers()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+
 
 
