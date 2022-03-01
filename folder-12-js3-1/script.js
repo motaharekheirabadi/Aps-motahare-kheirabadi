b36eec49cfee0058e97e9baefc5649114dc43f08
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-03-01 23:18:37 +0330

    addput

--- folder-12-js3-1/script.js
+++ folder-12-js3-1/script.js
@@ -87,4 +87,43 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
 
   /////////////////////////////////////////////////////////////
 
+  //   PUT
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "PUT",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    id: 1,
+    title: "karkhaneh",
+    body: "bar",
+    userId: 1,
+  }),
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "DELETE",
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "PATCH",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    name: "mohammadhossein",
+  }),
+});
+
+const arad = {
+  name: "mohammadhossein",
+  age: 15,
+  height: "tall",
+};
+
+
 
